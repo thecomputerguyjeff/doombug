b7732112c00eb44e22f2b8f2f2bac31b35485805
Jeffrey Mayer <jmayu@allstate.com>
2021-02-14 18:42:05 -0600

    add dev env file

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          hi
           <TestComponent />
         </p>
         <a
