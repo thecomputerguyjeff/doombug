1237aec952791dd423b8806cdbb92e1c71bee1d4
SiviaH <sivia.hellman@ti.htc.edu>
2021-02-17 10:37:10 -0600

    added login package to server package. The login method returns success or failure depending on the credentials entered as a body in Postman.

---
+++ server/src/main/java/com/ti/server/login/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.ti.server.login.controller;
+
+import com.ti.server.login.model.UserPass;
+import com.ti.server.login.service.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor
+@RequestMapping("/api/v1")
+public class LoginController {
+    private final LoginService loginService;
+    @PostMapping("/login")
+    public Object getPrimaryKey(@RequestBody UserPass userPass) {
+
+        return loginService.login(userPass);
+
+    }
+}
