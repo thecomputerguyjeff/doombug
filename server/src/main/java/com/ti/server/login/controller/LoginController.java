1e8b2c1e141e3c402b01dd6e9bb9ac64c9be5dcc
SiviaH <sivia.hellman@ti.htc.edu>
2021-02-17 10:52:31 -0600

    [#176936468] added @crossOrigin to LoginController

--- server/src/main/java/com/ti/server/login/controller/LoginController.java
+++ server/src/main/java/com/ti/server/login/controller/LoginController.java
@@ -5,12 +5,14 @@ import com.ti.server.login.service.LoginService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
 @RequestMapping("/api/v1")
+@CrossOrigin(originPatterns = {"*localhost*", "/"})
 public class LoginController {
     private final LoginService loginService;
     @PostMapping("/login")
