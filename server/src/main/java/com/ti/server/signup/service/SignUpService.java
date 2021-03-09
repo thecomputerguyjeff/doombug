4e69abab6cd9f54b01da6fc82ee361bf97aa3e4a
Devorah Levi <devorahlevi1@gmail.com>
2021-03-09 14:24:54 -0600

    [#177237217] Created Endpoint to Save User SignUp Info to the Database.

---
+++ server/src/main/java/com/ti/server/signup/service/SignUpService.java
@@ -0,0 +1,25 @@
+package com.ti.server.signup.service;
+
+import com.ti.server.user.entity.UserEntity;
+import com.ti.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignUpService
+{
+    private final UserRepository userRepository;
+
+    public ResponseEntity<Object> saveUserInfo(UserEntity userEntity) {
+
+        try {
+            userRepository.save(userEntity);
+            return new ResponseEntity<>("User has been successfully signed up and saved in the database.", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error. User info wasn't saved and user has not been signed up.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
