72ad371e7990ce553cf3d76e25922e71fb144f52
DevorahLevi <60837919+DevorahLevi@users.noreply.github.com>
2021-05-04 23:00:19 -0500

    [#177427478] Created endpoint to retrieve all posts from the database with the newest posts appearing first in the list. (#42)

--- server/src/main/java/com/ti/server/posts/repository/PostRepository.java
+++ server/src/main/java/com/ti/server/posts/repository/PostRepository.java
@@ -4,8 +4,10 @@ import com.ti.server.posts.entity.PostEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<PostEntity, String>
 {
-
+    public List<PostEntity> getPostEntitiesByOrderByCreateDateDesc();
 }
