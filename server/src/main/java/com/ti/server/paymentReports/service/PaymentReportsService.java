5b81b27ece2c803df1aaeed970b30b422ac547ed
Devorah Levi <devorahlevi1@gmail.com>
2021-05-13 21:34:34 -0500

    [#177900509] Set the limit for how many reports are returned.

--- server/src/main/java/com/ti/server/paymentReports/service/PaymentReportsService.java
+++ server/src/main/java/com/ti/server/paymentReports/service/PaymentReportsService.java
@@ -4,12 +4,13 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.BalanceTransaction;
 import com.stripe.model.BalanceTransactionCollection;
-import com.stripe.param.BalanceTransactionListParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +20,8 @@ public class PaymentReportsService
 
         Stripe.apiKey = "sk_test_51If5WfAvUyzs1dkX4OidydSPFFj4acLAx3zgZ3l2qKy08P0cHuGG5zuhuPL4rOv9mkZAnhgSyogqX2C8hHX5gZ4Q00dLcKGpqB";
 
-        BalanceTransactionListParams balanceTransactionListParams = BalanceTransactionListParams
-                .builder()
-                .build();
+        Map<String, Object> balanceTransactionListParams = new HashMap<>();
+        balanceTransactionListParams.put("limit", 100);
         BalanceTransactionCollection balanceTransactions = BalanceTransaction.list(balanceTransactionListParams);
 
         return new ArrayList<>(balanceTransactions.getData());
